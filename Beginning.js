8daf9b13511dfca94cacbe95026d85a652ac4d14
Ahmed <ahmedhamedaly19@gmail.com>
2019-12-30 16:38:32 +0000

    added constructor and clear

---
+++ Beginning.js
@@ -0,0 +1,14 @@
+class Calculator {
+    constructor(previousOperandTextElement, currentOperandTextElement) {
+        this.previousOperandTextElement = previousOperandTextElement;
+        this.currentOperandTextElement = currentOperandTextElement;
+        this.clear()
+    }
+
+    clear() {
+        this.currentOperand = '';
+        this.previousOperand = '';
+        this.operation = undefined
+    }
+
+}
